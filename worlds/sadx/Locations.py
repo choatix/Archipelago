cfedb5bb541ce01e0409e8edd20a3190214ce258
Classic <ClassicSpeed@users.noreply.github.com>
2024-07-04 00:17:00 -0300

    SADX: Added story unlock dependencies on Light Shoes

--- worlds/sadx/Locations.py
+++ worlds/sadx/Locations.py
@@ -9,10 +9,10 @@ class LocationInfo(TypedDict):
 
 location_table: List[LocationInfo] = [
     {"id": 2, "name": "Unlock Tails' Story", "needItem": -1},
-    {"id": 3, "name": "Unlock Knuckles' Story", "needItem": -1},
-    {"id": 4, "name": "Unlock Amy's Story", "needItem": -1},
-    {"id": 5, "name": "Unlock Gamma's Story", "needItem": -1},
-    {"id": 6, "name": "Unlock Big's Story", "needItem": -1},
+    {"id": 3, "name": "Unlock Knuckles' Story", "needItem": 10},
+    {"id": 4, "name": "Unlock Amy's Story", "needItem": 10},
+    {"id": 5, "name": "Unlock Gamma's Story", "needItem": 10},
+    {"id": 6, "name": "Unlock Big's Story", "needItem": 10},
 
     {"id": 10, "name": "Light shoes upgrade (Sonic)", "needItem": -1},
     {"id": 11, "name": "Crystal ring upgrade (Sonic)", "needItem": 10},
