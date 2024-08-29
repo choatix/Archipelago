93cb7f303ccec9b7af510044b3d097a0b960d752
Classic <ClassicSpeed@users.noreply.github.com>
2024-08-28 23:32:18 -0300

    SADX: Added Light Shoes for Sonic's Red Mountain Life capsules

--- worlds/sadx/__init__.py
+++ worlds/sadx/__init__.py
@@ -100,7 +100,7 @@ class SonicAdventureDXWorld(World):
 
     def fill_slot_data(self) -> Dict[str, Any]:
         return {
-            "ModVersion": 80,
+            "ModVersion": 81,
             "Goal": self.options.goal.value,
             "EmblemsForPerfectChaos": self.item_distribution.emblem_count_progressive,
             "StartingCharacter": self.starter_setup.character.value,
