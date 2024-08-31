bf6f5d3d177d34b95e859ffdbd68ff13f1770e3c
Classic <ClassicSpeed@users.noreply.github.com>
2024-08-31 18:29:29 -0300

    SADX: Improved wording on options

--- worlds/sadx/Options.py
+++ worlds/sadx/Options.py
@@ -6,12 +6,12 @@ from Options import PerGameCommonOptions
 
 class Goal(Choice):
     """
-    Determines the goal of the seed
-    Levels (0): You have to complete all the action stages available to unlock the Perfect Chaos Fight.
-    Emblems (1): You have to collect a certain number of emblems to unlock the Perfect Chaos Fight.
-    Chaos Emerald Hunt (2): You have to collect all 7 Chaos Emeralds to unlock the Perfect Chaos Fight.
-    Levels and Chaos Emerald Hunt (3): You have to beat all the action stages and collect the emeralds to fight Perfect Chaos.
-    Emblems and Chaos Emerald Hunt (4): You have to collect both emblems and the emeralds to fight Perfect Chaos.
+    Determines the goal of the seed.
+    Levels (0): Complete all the action stages available to unlock the Perfect Chaos Fight.
+    Emblems (1): Collect a certain number of emblems to unlock the Perfect Chaos Fight.
+    Chaos Emerald Hunt (2): Collect all 7 Chaos Emeralds to unlock the Perfect Chaos Fight.
+    Levels and Chaos Emerald Hunt (3): Beat all the action stages and collect the emeralds to fight Perfect Chaos.
+    Emblems and Chaos Emerald Hunt (4): Collect both emblems and the emeralds to fight Perfect Chaos.
 
     Keep in mind selecting emerald hunt will require enough checks to add the 7 emeralds to the pool.
     Also, selecting emblems will require at least 5 checks to add the 5 emblems to the pool.
@@ -28,10 +28,10 @@ class Goal(Choice):
 
 class LogicLevel(Choice):
     """
-    What kind of logic the randomizer will use
+    Determines the logic the randomizer will use.
     Normal Logic (0): Very forgiving, ideal if you are not used to this game or its location checks.
-    Hard Logic (1): Less forgiving logic, some checks require to do spindash jumps or dying to get the check.
-    Expert Logic (2): The most unforgiving logic, some checks require to do out of bounds jumps.
+    Hard Logic (1): Less forgiving logic, some checks require performing spindash jumps or dying to get the check.
+    Expert Logic (2): The most unforgiving logic, some checks require performing out-of-bounds jumps.
     """
     display_name = "Logic Level"
     option_normal_logic = 0
@@ -41,7 +41,7 @@ class LogicLevel(Choice):
 
 
 class EmblemPercentage(Range):
-    """What percentage of the available emblems do you need to unlock the final story"""
+    """Percentage of the available emblems needed to unlock the final story."""
     display_name = "Emblem Requirement Percentage"
     range_start = 1
     range_end = 80
@@ -49,28 +49,28 @@ class EmblemPercentage(Range):
 
 
 class RandomStartingLocation(DefaultOnToggle):
-    """Randomize starting location, if false, you will start at Station Square"""
-    display_name = "Random Starting location"
+    """Randomize starting location. If false, you will start at Station Square."""
+    display_name = "Random Starting Location"
 
 
 class RandomStartingLocationPerCharacter(DefaultOnToggle):
-    """If Randomize starting location is enabled, each character will start in a random location"""
-    display_name = "Random Starting location per character"
+    """If randomize starting location is enabled, each character will start in a random location."""
+    display_name = "Random Starting Location Per Character"
 
 
 class GuaranteedLevel(Toggle):
-    """Ensures access to a level from the start, even if it means giving you an item"""
+    """Ensures access to a level from the start, even if it means giving you an item."""
     display_name = "Guaranteed Level Access"
 
 
 class EntranceRandomizer(Toggle):
-    """ Randomizes the entrance of action stages """
+    """Randomizes the entrance of action stages."""
     display_name = "Entrance Randomizer"
 
 
 class RingLink(Toggle):
     """
-    Whether your in-level ring gain/loss is linked to other players
+    Whether your in-level ring gain/loss is linked to other players.
     """
     display_name = "Ring Link"
 
@@ -78,17 +78,17 @@ class RingLink(Toggle):
 class HardRingLink(Toggle):
     """
     If Ring Link is enabled, sends and receives rings in more situations.
-    Particularly it will subtract rings when finishing a level and during the Perfect Chaos fight.
+    Particularly, it will subtract rings when finishing a level and during the Perfect Chaos fight.
     """
     display_name = "Hard Ring Link"
 
 
 class RingLoss(Choice):
     """
-    How taking damage is handled
-    Classic: You lose all of your rings when hit
-    Modern: You lose 20 rings when hit
-    One Hit K.O.: You die immediately when hit
+    How taking damage is handled.
+    Classic: You lose all of your rings when hit.
+    Modern: You lose 20 rings when hit.
+    One Hit K.O.: You die immediately when hit.
     """
     display_name = "Ring Loss"
     option_classic = 0
@@ -98,42 +98,42 @@ class RingLoss(Choice):
 
 
 class PlayableSonic(DefaultOnToggle):
-    """Determines whether Sonic is playable"""
+    """Determines whether Sonic is playable."""
     display_name = "Playable Sonic"
 
 
 class PlayableTails(DefaultOnToggle):
-    """Determines whether Tails is playable"""
+    """Determines whether Tails is playable."""
     display_name = "Playable Tails"
 
 
 class PlayableKnuckles(DefaultOnToggle):
-    """Determines whether Knuckles is playable"""
+    """Determines whether Knuckles is playable."""
     display_name = "Playable Knuckles"
 
 
 class PlayableAmy(DefaultOnToggle):
-    """Determines whether Amy is playable"""
+    """Determines whether Amy is playable."""
     display_name = "Playable Amy"
 
 
 class PlayableGamma(DefaultOnToggle):
-    """Determines whether Gamma is playable"""
+    """Determines whether Gamma is playable."""
     display_name = "Playable Gamma"
 
 
 class PlayableBig(DefaultOnToggle):
-    """Determines whether Big is playable"""
+    """Determines whether Big is playable."""
     display_name = "Playable Big"
 
 
 class BaseActionStageMissionChoice(Choice):
     """
-        For missions, the options go from 3 to 0
-        3 means Missions A, B and C
-        2 means Missions B and C
-        1 means Missions C
-        0 means no missions at all (You can still play the character if they are enabled)
+        For missions, the options range from 3 to 0.
+        3 means Missions A, B, and C.
+        2 means Missions B and C.
+        1 means Mission C.
+        0 means no missions at all (You can still play the character if they are enabled).
     """
     option_none = 0
     option_c = 1
@@ -149,12 +149,12 @@ class SonicActionStageMissions(BaseActionStageMissionChoice):
 
 class TailsActionStageMissions(BaseActionStageMissionChoice):
     """Choose what action stage missions will be a location check for Tails."""
-    display_name = "Tail's Action Stage Missions"
+    display_name = "Tails' Action Stage Missions"
 
 
 class KnucklesActionStageMissions(BaseActionStageMissionChoice):
     """Choose what action stage missions will be a location check for Knuckles."""
-    display_name = "Knuckles's Action Stage Missions"
+    display_name = "Knuckles' Action Stage Missions"
 
 
 class AmyActionStageMissions(BaseActionStageMissionChoice):
@@ -173,64 +173,62 @@ class BigActionStageMissions(BaseActionStageMissionChoice):
 
 
 class RandomizedSonicUpgrades(DefaultOnToggle):
-    """Determines whether Sonic's upgrades are randomized and sent to the item pool"""
+    """Determines whether Sonic's upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Sonic's Upgrades"
 
 
 class RandomizedTailsUpgrades(DefaultOnToggle):
-    """Determines whether Tails' upgrades are randomized and sent to the item pool"""
+    """Determines whether Tails' upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Tails' Upgrades"
 
 
 class RandomizedKnucklesUpgrades(DefaultOnToggle):
-    """Determines whether Knuckles' upgrades are randomized and sent to the item pool"""
+    """Determines whether Knuckles' upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Knuckles' Upgrades"
 
 
 class RandomizedAmyUpgrades(DefaultOnToggle):
-    """Determines whether Amy's upgrades are randomized and sent to the item pool"""
+    """Determines whether Amy's upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Amy's Upgrades"
 
 
 class RandomizedBigUpgrades(DefaultOnToggle):
-    """Determines whether Big's upgrades are randomized and sent to the item pool"""
+    """Determines whether Big's upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Big's Upgrades"
 
 
 class RandomizedGammaUpgrades(DefaultOnToggle):
-    """Determines whether Gamma's upgrades are randomized and sent to the item pool"""
+    """Determines whether Gamma's upgrades are randomized and sent to the item pool."""
     display_name = "Randomize Gamma's Upgrades"
 
 
 class BossChecks(DefaultOnToggle):
-    """Determines whether beating a boss grants a check
-    (15 Locations)"""
+    """Determines whether beating a boss grants a check (15 Locations)."""
     display_name = "Boss Checks"
 
 
 class UnifyChaos4(DefaultOnToggle):
-    """Determines whether the Chaos 4 fight counts as a single location or three (Sonic, Tails and Knuckles)"""
+    """Determines whether the Chaos 4 fight counts as a single location or three (Sonic, Tails, and Knuckles)."""
     display_name = "Unify Chaos 4"
 
 
 class UnifyChaos6(Toggle):
-    """Determines whether the Chaos 6 fight counts as a single location or three (Sonic, Big and Knuckles)"""
+    """Determines whether the Chaos 6 fight counts as a single location or three (Sonic, Big, and Knuckles)."""
     display_name = "Unify Chaos 6"
 
 
 class UnifyEggHornet(Toggle):
-    """Determines whether the Egg Hornet fight counts as a single location or two (Sonic, Tails)"""
+    """Determines whether the Egg Hornet fight counts as a single location or two (Sonic, Tails)."""
     display_name = "Unify Egg Hornet"
 
 
 class FieldEmblemsChecks(DefaultOnToggle):
-    """Determines whether collecting field emblems grants checks
-    (12 Locations)"""
+    """Determines whether collecting field emblems grants checks (12 Locations)."""
     display_name = "Field Emblems Checks"
 
 
 class MissionChecks(Toggle):
-    """Determines whether completing missions grants checks (60 Locations)"""
+    """Determines whether completing missions grants checks (60 Locations)."""
     display_name = "Enable Mission Checks"
 
 
@@ -240,11 +238,11 @@ class AutoStartMissions(Toggle):
 
 
 class MissionBlackList(OptionSet):
-    """Determines what missions are blacklisted, the default are:
-    Mission 49 (Flags in the Kart section of Twinkle Park )
-    Mission 53 (Triple Jump in the Snowboard section of Ice Cap)
-    Mission 54 (Flags in the Snowboard section of Ice Cap)
-    Mission 58 (Flags in the rolling bounce section of Lost World)
+    """Determines what missions are blacklisted. The default are:
+    Mission 49 (Flags in the Kart section of Twinkle Park).
+    Mission 53 (Triple Jump in the Snowboard section of Ice Cap).
+    Mission 54 (Flags in the Snowboard section of Ice Cap).
+    Mission 58 (Flags in the rolling bounce section of Lost World).
     """
     display_name = "Mission Blacklist"
     default = {'49', '53', '54', '58'}
@@ -252,76 +250,74 @@ class MissionBlackList(OptionSet):
 
 
 class SubLevelChecks(DefaultOnToggle):
-    """Determines whether beating Twinkle Circuit and Sand Hill grants checks (2 Locations)"""
+    """Determines whether beating Twinkle Circuit and Sand Hill grants checks (2 Locations)."""
     display_name = "Sub-Level Checks"
 
 
 class SubLevelChecksHard(Toggle):
     """
-    Determines whether beating the harder (points based) Twinkle Circuit and Sand Hill missions grants checks (2 Locations)
-    Only works if sublevel checks are enabled
+    Determines whether beating the harder (points-based) Twinkle Circuit and Sand Hill missions grants checks (2 Locations).
+    Only works if sublevel checks are enabled.
     """
-    display_name = "Sub-Level Checks"
+    display_name = "Hard Sub-Level Checks"
 
 
 class SkyChaseChecks(DefaultOnToggle):
-    """Determines whether beating Sky Chase Act 1 and 2 grants checks (2 Locations)"""
-    display_name = "Sub-Level Checks"
+    """Determines whether beating Sky Chase Act 1 and 2 grants checks (2 Locations)."""
+    display_name = "Sky Chase Checks"
 
 
 class SkyChaseChecksHard(Toggle):
     """
-    Determines whether beating the harder (points based) Sky Chase Act 1 and 2 mission grants checks (2 Locations)
-    Only works if Sky Chase checks are enabled
+    Determines whether beating the harder (points-based) Sky Chase Act 1 and 2 missions grants checks (2 Locations).
+    Only works if Sky Chase checks are enabled.
     """
-    display_name = "Sub-Level Checks"
+    display_name = "Hard Sky Chase Checks"
 
 
 class LifeSanity(Toggle):
-    """Determines whether collecting life capsules grants checks
-    (102 Locations)"""
+    """Determines whether collecting life capsules grants checks (102 Locations)."""
     display_name = "Life Sanity"
 
 
 class PinballLifeCapsules(Toggle):
-    """Determines whether pinball's life capsules grant checks
-    (2 Locations)"""
-    display_name = "Include pinball's Life Capsules"
+    """Determines whether pinball's life capsules grant checks (2 Locations)."""
+    display_name = "Include Pinball's Life Capsules"
 
 
 class SonicLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Sonic's life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Sonic's life capsules are part of the randomizer."""
     display_name = "Sonic's Life Sanity"
 
 
 class TailsLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Tails' life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Tails' life capsules are part of the randomizer."""
     display_name = "Tails' Life Sanity"
 
 
 class KnucklesLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Knuckles' life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Knuckles' life capsules are part of the randomizer."""
     display_name = "Knuckles' Life Sanity"
 
 
 class AmyLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Amy's life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Amy's life capsules are part of the randomizer."""
     display_name = "Amy's Life Sanity"
 
 
 class BigLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Big's life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Big's life capsules are part of the randomizer."""
     display_name = "Big's Life Sanity"
 
 
 class GammaLifeSanity(DefaultOnToggle):
-    """If life-sanity is on, determines whether Gamma's life capsules are part of the randomizer"""
+    """If life-sanity is on, determines whether Gamma's life capsules are part of the randomizer."""
     display_name = "Gamma's Life Sanity"
 
 
 class JunkFillPercentage(Range):
     """
-    Replace a percentage of non-required emblems in the item pool with random junk items
+    Replace a percentage of non-required emblems in the item pool with random junk items.
     """
     display_name = "Junk Fill Percentage"
     range_start = 0
@@ -331,7 +327,7 @@ class JunkFillPercentage(Range):
 
 class TrapFillPercentage(Range):
     """
-    Replace a percentage of junk items in the item pool with random traps
+    Replace a percentage of junk items in the item pool with random traps.
     """
     display_name = "Trap Fill Percentage"
     range_start = 0
@@ -341,8 +337,8 @@ class TrapFillPercentage(Range):
 
 class BaseTrapWeight(Choice):
     """
-    Base Class for Trap Weights
-    The available options are 0 (off), 1 (low), 2 (medium) and 4 (high)
+    Base class for trap weights.
+    The available options are 0 (off), 1 (low), 2 (medium), and 4 (high).
     """
     option_none = 0
     option_low = 1
@@ -353,28 +349,28 @@ class BaseTrapWeight(Choice):
 
 class IceTrapWeight(BaseTrapWeight):
     """
-    Likelihood of receiving a trap which freeze the player in place
+    Likelihood of receiving a trap that freezes the player in place.
     """
     display_name = "Ice Trap Weight"
 
 
 class SpringTrapWeight(BaseTrapWeight):
     """
-    Likelihood of receiving a trap which spawns a spring that sends the player flying to the opposite direction
+    Likelihood of receiving a trap that spawns a spring that sends the player flying in the opposite direction.
     """
     display_name = "Spring Trap Weight"
 
 
 class PoliceTrapWeight(BaseTrapWeight):
     """
-    Likelihood of receiving a trap which spawns a lot of Cop Speeder enemies
+    Likelihood of receiving a trap that spawns a lot of Cop Speeder enemies.
     """
     display_name = "Police Trap Weight"
 
 
 class BuyonTrapWeight(BaseTrapWeight):
     """
-    Likelihood of receiving a trap which spawns a lot of Buyon enemies
+    Likelihood of receiving a trap that spawns a lot of Buyon enemies.
     """
     display_name = "Buyon Trap Weight"
 
