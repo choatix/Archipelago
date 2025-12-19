3f58fa655c0ac11b9c64fe6b7dd0ddb7bd8e945b
choatix <choatixtherobot@hotmail.com>
2025-12-19 08:19:11 +0000

    Resolve deleted function.

--- worlds/shadow_the_hedgehog/ShTHClient.py
+++ worlds/shadow_the_hedgehog/ShTHClient.py
@@ -278,13 +278,13 @@ class ShTHCommandProcessor(ClientCommandProcessor):
     #    self.reports.append((arguments["i"], self.region))
 
 
-    #def arg_finish(self, build, type, results, value):
-    ##    to_use = " ".join(build)
+    def arg_finish(self, build, type, results, value):
+        to_use = " ".join(build)
 
-    #   if value:
-    #        results[type] = to_use
-    #    else:
-    #        results[type+to_use] = True
+        if value:
+            results[type] = to_use
+        else:
+            results[type+to_use] = True
 
     def parse_args(self, args, value=True):
         arguments = {}
